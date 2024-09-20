9ebb4485ba3eb7dbb843d5be9e09f5456ffe75a3
Rashmi Ranjan Das <rashmirdas001@gmail.com>
2024-09-20 14:15:52 +0000

    Add remove todo feature.

--- app.js
+++ app.js
@@ -10,12 +10,31 @@ function addTodo() {
   }
 }
 
+function removeTodo (index){
+    todos.splice(index, 1);
+    renderTodos();
+}
+
 function renderTodos() {
   const list = document.getElementById("todo-list");
   list.innerHTML = "";
-  todos.forEach((todo) => {
+  todos.forEach((todo , index) => {
     const listItem = document.createElement("li");
     listItem.textContent = todo;
+
+    // cReate remove button for each todo
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Delete";
+    removeBtn.style.marginLeft = "10px";
+    removeBtn.style.border = "2px solid red";
+    removeBtn.style.backgroundColor = "red"
+    removeBtn.style.color = "white";
+    removeBtn.style.fontSize = "15px";
+    removeBtn.onclick = function () {
+      removeTodo(index);
+    };
+    listItem.appendChild(removeBtn);
+
     list.appendChild(listItem);
   });
-};
+}
